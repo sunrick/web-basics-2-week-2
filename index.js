043a496becd76e918e6ddebf57c77bf490e32485
Elizabeth Thompson <lizthompson14@gmail.com>
2020-10-22 12:45:12 -0400

    WIP

--- index.js
+++ index.js
@@ -1,89 +1,65 @@
+/* Challenge #1
+ * Create a function that accepts two number arguments and returns the product
+ *
+ * Steps:
+ * 1. Create a function called `multiply`
+ * 2. Add two parameters in your function signature, `num1` and `num2`
+ * 3. Write logic to find the product of `num1` and `num2`, but you must do so without using the `*` operator
+ * 4. Add a return statement in your function which returns the `product`
+ * 7. Use `console.log` to print out the output of `multiply()`
+ *
+ * Hint: multiplication is a sequence of sums (2 * 3 = 2 + 2 + 2)
+ */
 
-//------------------------ Question 1---------------------------
-//Write a function that ask the user for two numbers and return the product of them. But the challenge here is that you can't use the operator * (you can't make the product)
-//hint: multiplication is a sequence of sums
+// TODO: ADD CODE HERE
 
- //@param {number} number1
- //@param {number} number2
- //@return {number} product
-const multiplication = () => {
-  return
-}
+/* Challenge #2
+ * ...
+ *
+ * Steps:
+ * 1. ...
+ *
+ * Hint: ...
+ */
 
-const answer1 = multiplication()
-console.log('answer1', answer1)
+// TODO: ADD CODE HERE
 
-const htmlTarget = document.getElementById('a-1')
-htmlTarget.innerHTML = answer1
+/* Challenge #3
+ * ...
+ *
+ * Steps:
+ * 1. ...
+ *
+ * Hint: ...
+ */
 
+// TODO: ADD CODE HERE
 
+/* Challenge #4
+ * ...
+ *
+ * Steps:
+ * 1. ...
+ *
+ * Hint: ...
+ */
 
-//------------------------ Question 2 ---------------------------
-// Write a function that recieves a string as a parameter and evaluate each character of the string to determinate if the character is vowel or a consonant. you have to store each character on separates arrays, one for vowels and the other one for consonants.
-// after separating the characters concatenate both arrays. ask the user if wants the vowels first or consonants first in the final array.
-//hint:
+const library = [
+  {
+    title: 'The Road Ahead',
+    author: 'Bill Gates',
+    libraryID: 1254,
+  },
+  {
+    title: 'Walter Isaacson',
+    author: 'Steve Jobs',
+    libraryID: 4264,
+  },
+  {
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    author: 'Suzanne Collins',
+    libraryID: 3245,
+  },
+]
 
-//@param {string}
-//@return {character} => array of characters
-vowelOrConsonant = () => {
-  return
-}
-
-const answer2 = vowelOrConsonant()
-
-const htmlTarget2 = document.getElementById('a-2')
-htmlTarget2.innerHTML = answer2
-
-
-
-
-//------------------------ Question 3 ---------------------------
-//Now let's create a small game. The game consists in a player (ask the user for the name).The player has 3 oportunities to guess a number. The number is a random number between 10 - 50.
-//If the player don't find the number, the program must displays an alert and stop the game, but if the player finds the number, then the program must show a congratulations message (alert) with the name of the player in upperCase letters and stop the game
-//You must have to store the player information in a 'player' object. The Player object contains the following Properties:
-// {string} name, {number} lives, {numbers} fail_numbers[]
-//where: name, saves the name of the player. Lives, represents the remaining oportunities each time the player fails. Fail_numbers, is an array of numbers that stores the fail numbers the player has used
-
-//@return {string} win / gameOver => the string that says if the user wasted the three oportunities showing the fails numbers or the name if the player wins
-guessTheNumber  = () => {
-  return
-}
-
-const answer3 = guessTheNumber()
-
-const htmlTarget3 = document.getElementById('a-3')
-htmlTarget3.innerHTML = answer3
-
-
-
-//------------------------ Question 4 ---------------------------
-// In the function below we are giving you an array of objects, each one with the same properties. Ask to the user for 3 diferentes options to sorting the array from the highest to lowest. In the case of a string, the criteria to sort must be the length of the string. The first one is sorting the array of objects based on the title property.
-// The second one sorting the array of objects based on the author property, the third one based on the library property. finally, the return value has to be the string sorted of the property selected separeted with a semicolon. Remember you have to sort all of the array based on the selected property
-//example: if the user select sorting by title the return value must be: "Mockingjay: The Final Book of The Hunger Games; Walter Isaacson; The Road Ahead"
-
-sort = () => {
-
-  var library = [
-   {
-       title:  'The Road Ahead',
-       author: 'Bill Gates',
-       libraryID: 1254
-   },
-   {
-       title: 'Walter Isaacson',
-       author: 'Steve Jobs',
-       libraryID: 4264
-   },
-   {
-       title: 'Mockingjay: The Final Book of The Hunger Games',
-       author: 'Suzanne Collins',
-       libraryID: 3245
-   }];
-
-  return
-}
-
-const answer4 = sort()
-
-const htmlTarget4 = document.getElementById('a-4')
-htmlTarget4.innerHTML = answer4
+// TODO: ADD CODE HERE
